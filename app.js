9dbd0aed3b35a53791f1c673fd535290fb5c7e07
minhacontateste <79100860+minhacontateste@users.noreply.github.com>
2021-02-15 12:47:01 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+function verificar(){
+    let data = new Date()
+    let ano = data.getFullYear()
+    let anoInput = window.document.getElementById('birthyear')
+    let res = document.getElementById('textoPreencherCampos')  
+    let img = document.getElementById("foto")
+    if(anoInput.value.length == 0 || anoInput.value > ano){
+        window.alert("[ERRO] Verifique os dados e tente novamente!.")
+    } else {
+        let fsex = window.document.getElementsByName('gender')
+        let idade = ano - anoInput.value
+        var genero = fsex[0].checked ? 'Homem':'Mulher'
+        
+        if(genero === "Homem" && idade <= 12){
+            img.src = "./images/foto-bebe-m.png"
+        } else if(genero === "Homem" && idade <= 30 && idade >= 13){
+            img.src = "./images/foto-jovem-m.png"
+        } else if(genero === "Homem" && idade > 30 && idade < 65){
+            img.src = "./images/foto-adulto-m.png"
+        } else if(genero === "Homem" && idade >= 65){
+            img.src = "./images/foto-idoso-m.png"
+        }
+
+        if(genero === "Mulher" && idade <= 12){
+            img.src = "./images/foto-bebe-f.png"
+        } else if(genero === "Mulher" && idade <= 30 && idade >= 13){
+            img.src = "./images/foto-jovem-f.png"
+        } else if(genero === "Mulher" && idade > 30 && idade < 65){
+            img.src = "./images/foto-adulto-f.png"
+        } else if(genero === "Mulher" && idade >= 65){
+            img.src = "./images/foto-idoso-f.png"
+        }
+
+        if(idade < 105){
+            res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        } else{
+            res.innerHTML = `Detectamos Dinossauro com ${idade} anos.`
+            img.src = "./images/dino.png"
+        }
+    }
+    
+}
